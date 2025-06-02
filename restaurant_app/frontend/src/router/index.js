0e49e107092a0ee576faabb55369338266f709db
Mike Paluchowski <mikolaj.paluchowski1@gmail.com>
2025-06-02 22:13:13 +0200

    minor changes in frontend

--- restaurant_app/frontend/src/router/index.js
+++ restaurant_app/frontend/src/router/index.js
@@ -9,12 +9,20 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/menu',
+    name: 'menu',
+    component: () => import('../views/MenuView.vue')
+  },
+    {
+      //this is for "my orders" page
+    path: '/order',
+    name: 'order',
+    component: () => import('../views/OrderView.vue')
+  },
+    {
+    path: '/cart',
+    name: 'cart',
+    component: () => import('../views/CartView.vue')
   }
 ]
 
---
+++ restaurant_app/frontend/src/views/OrderView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="order">
+    <h1>This is a "track order" page</h1>
+  </div>
+</template>
